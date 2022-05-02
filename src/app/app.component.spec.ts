e6945e130e241c5522a53c8b007594d032c5bc78
Jose Manuel Vega <jm.vegaa1@uniandes.edu.co>
2022-05-01 21:26:30 -0500

    Se configuran los test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http'
+import  { VehiculoListaComponent } from './vehiculo/vehiculo-lista/vehiculo-lista.component';
+
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
-      declarations: [AppComponent],
+      imports: [RouterTestingModule,HttpClientModule],
+      declarations: [AppComponent,VehiculoListaComponent],
     }).compileComponents();
   });
 
@@ -22,12 +25,5 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('base-project');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'base-project app is running!'
-    );
-  });
+
 });
--- src/app/vehiculo/vehiculo-lista/vehiculo-lista.component.spec.ts
+++ src/app/vehiculo/vehiculo-lista/vehiculo-lista.component.spec.ts
@@ -2,15 +2,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { faker } from '@faker-js/faker';
 import { VehiculoListaComponent } from './vehiculo-lista.component';
+import { Vehiculo } from '../vehiculo';
 
 describe('VehiculoListaComponent', () => {
   let component: VehiculoListaComponent;
   let fixture: ComponentFixture<VehiculoListaComponent>;
+  let debug: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [HttpClientModule ],
       declarations: [ VehiculoListaComponent ]
     })
     .compileComponents();
@@ -19,10 +23,41 @@ describe('VehiculoListaComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(VehiculoListaComponent);
     component = fixture.componentInstance;
+
+    component.vehiculos = [
+      new Vehiculo(
+      90,
+      "CheVrolet",
+      "corsa",
+      "Active",
+      2019,
+      80000,
+      "gris",
+      faker.image.imageUrl()
+
+      ),
+    ];
     fixture.detectChanges();
+    debug = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  it('should display one vehicle', () => {
+    const vehicleNumbers= debug.queryAll(By.css('.table-group-item-action'));
+    expect(vehicleNumbers.length).toBe(0);
+  });
+
+
 });
+
+
+// id:number,
+//     marca:string,
+//     linea:string,
+//     referencia:string,
+//     modelo:number,
+//     kilometraje:number,
+//     color:string,
+//     imagen:String
